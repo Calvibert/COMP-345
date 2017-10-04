f07aec3f0bc3e51ca56d5e5e3755cb7165f150c3
Samuel Dufresne <samuel.dufresne@live.com>
2017-10-04 01:01:17 -0400

    Missing proper term incrementation

--- assignment1/src/main.cpp
+++ assignment1/src/main.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+// Structure used by the program to store term, document name and frequency data
 struct dictionary {
 	string term;
 	string docName;
@@ -30,28 +31,23 @@ struct dictionary {
 	}
 };
 
-// Open the main file and return the list of filenames contained within it
-vector<string> openingInput(string inputFile){
+// Read a file and return a vector<string> of the terms stored as a list.
+vector<string> readFile(string inputFile){
 	ifstream fin(inputFile);
 	vector<string> fileNames;
 	string currentLine;
 
-	if (!fin)
+	if (!fin) {
 		throw "Error opening input file. Closing";
+	}
 	
 	//Loop through file, read each line into the vector
-	while (getline(fin, currentLine))
+	while (getline(fin, currentLine)) {
 		fileNames.push_back(currentLine);
+	}
 
 	fin.close();
-
 	return fileNames; 
-}
-
-// If a word already exists, increment its frequency by 1
-void increment(vector<dictionary>* tokens, dictionary currentDict, int index) {
-	++currentDict.freq;
-	(*tokens)[index] = currentDict;
 }
 
 // If a word contains a character specified in the array, it will remove it.
@@ -59,6 +55,12 @@ string containsPunctuation(string word) {
 	word.erase(std::remove_if(word.begin(), word.end(), ::ispunct), word.end());
 	return word;
 }
+
+void increment(vector<dictionary> &tokens, dictionary &currentDict, int index) {
+    currentDict.freq = currentDict.freq + 1;
+    tokens[index] = currentDict;
+}
+
 
 // Given a term-frequency vector, filter out words matching a certain list.
 bool isStopWord(string word, vector<string> stopWords) {
@@ -90,7 +92,9 @@ vector<dictionary> sortDict(vector<dictionary> dict) {
 }
 
 // Go through a file and count the frequency of each term accordingly
-void openingFiles(string inputFile, vector<string> stopWordVector,
+// Maintains 2 vector<dictionary>. One is stopword-filtered, the other is not.
+// Maintains 2 ints of the longest term in each vector<dictionary>
+void processFile(string inputFile, vector<string> stopWordVector,
 	string::size_type & longestWord, string::size_type & longestFilteredWord,
 	vector<dictionary> & allTokens, vector<dictionary> & filteredTokens) {
 
@@ -104,21 +108,23 @@ void openingFiles(string inputFile, vector<string> stopWordVector,
 
 	//Loop through file, read each word into the vector of tokens
 	while (fin >> currentWord) {
-		// remove punctuation
+		// Remove punctuation using a helper method
 		currentWord = containsPunctuation(currentWord);
-		// to lower case
+		// Transform term to lower case using built in method
 		transform(currentWord.begin(), currentWord.end(), currentWord.begin(), ::tolower);
 
+		// Update longestWord if necessary
 		if (currentWord.length() > longestWord) {
 			longestWord = currentWord.length();
 		}
 
+		//
 		temp.term = currentWord;
 		temp.docName = inputFile;
 		if (int index = find((allTokens).begin(), (allTokens).end(), temp) != (allTokens).end()) {
-			//increment(allTokens, temp, index);
+			increment(allTokens, temp, index);
 			if (isStopWord(currentWord, stopWordVector)) {
-				//increment(filteredTokens, temp, index);
+				increment(filteredTokens, temp, index);
 			}
 		}
 		else {
@@ -150,121 +156,144 @@ void openingFiles(string inputFile, vector<string> stopWordVector,
 
 }
 
-// Read the tokens from a file and return them as a vector of strings
-vector<string> readFile(string file) {
-	vector<string> tokens;
-	ifstream fin(file);
-	string currentWord;
-
-	while (fin >> currentWord) {
-		transform(currentWord.begin(), currentWord.end(), currentWord.begin(), ::tolower);
-		tokens.push_back(currentWord);
+int getTotalWidth(vector<string> fileNames, int longestWord) {
+	// Calculate the total width of the table:
+	int width = longestWord;
+	for (unsigned a = 0; a < fileNames.size(); a++) {
+		width = width + fileNames[a].length();
 	}
-
-	fin.close();
-
-	return tokens;
+	width = width + (3 * (fileNames.size() + 2) ) - 1;
+	return width;
 }
-
 
 void output(vector<dictionary> dict, vector<string> fileNames, int longestWord) {
 	string previousWord = "";
+	bool firstWord = true;
+	unsigned freqCount = 0;
+	unsigned totalWidth = getTotalWidth(fileNames, longestWord);
+
+
+	// If longest word is shorter than dictionary:
 	if (longestWord < 8) {
 		longestWord = 8;
 	}
-	// Print first line:
-	cout << "-------------------------------------------------------------------------------------------" << endl;
 
-	cout << "| Dictionary ";
+	// Print first line:
+	cout << " " << setfill('-') << setw(totalWidth) << " " << endl;
+	cout << setfill(' ');
+
+	cout << " | " << setw(longestWord) << "Dictionary";
 	// Print all file names after Dictionary
 	for (unsigned i = 0; i < fileNames.size(); i++) {
-		cout << setw(longestWord) << "| " << fileNames[i].c_str();
+		cout  << " | " << fileNames[i].c_str();
 	}
+	cout << " | ";
 
+	// Prints the terms and their frequency in each document
 	for (unsigned j = 0; j < dict.size(); j++) {
-		// If new term, print it
+		// If new term or first term, complete the line and start the new one
 		if (previousWord != dict[j].term) {
+			// Complete the previous row:
+			if (!firstWord) {
+				for (unsigned y = freqCount; y < fileNames.size(); y++) {
+					cout << " | "  << setw(fileNames[y].length()) << "0";
+					if (y == fileNames.size() - 1) {
+						cout << " | ";
+					}
+				}
+			}
+			firstWord = false;
+			// Reset row counter
+			freqCount = 0;
+			// Start new line
 			cout << endl;
-			cout << "| " << dict[j].term.c_str();
+			// Write term
+			cout << " | " << setw(longestWord)  << dict[j].term.c_str();
 		}
 
-		int freqPos = find(fileNames.begin(), fileNames.end(), dict[j].term) - fileNames.begin();
 
-		for(int y=0; y < freqPos; y++) {
-			cout << "| ";
-			if (freqPos == y) {
+		// Get the index of the term in fileNames vector
+		unsigned freqPos = find(fileNames.begin(), fileNames.end(), dict[j].docName) - fileNames.begin();
+
+		// Go through every fileName row until it reaches the position of the current element
+		for(unsigned y=freqCount; y < freqPos + 1; y++) {
+			cout << " | ";
+			if (y == freqPos) {
 				cout << setw(fileNames[y].length()) << dict[j].freq;
 			} else {
-				cout << setw(fileNames[y].length());
+				cout << setw(fileNames[y].length()) << "0";
+			}
+			freqCount++;
+
+			// last freq input of the line
+			if (y == (fileNames.size() - 1)) {
+				cout << " | ";
 			}
 		}
 
 		previousWord = dict[j].term;
 	}
+	// Finish padding the last line
+	for (unsigned y = freqCount; y < fileNames.size(); y++) {
+		cout << " | " << setw(fileNames[y].length()) << "0";
+		if (y == fileNames.size() - 1) {
+			cout << " | ";
+		}
+	}
 
-	cout << "-------------------------------------------------------------------------------------------" << endl;
+	cout << endl;
+	// Print last line
+	cout << " " << setfill('-') << setw(totalWidth) << " " << endl;
+	cout << setfill(' ');
 
 }
 
 int main(){
 	// PART 1: INPUT
-	//could replace with a cin asking for a file name, but for the purposes of this, we already know the file's name
+
 	string indexFile = "index.txt";
 	vector<string> fileNames;
 
 	try {
-		//this will populate the vector with every line in the file (ie every file name we'll be going to next)
-		fileNames = openingInput(indexFile);
+		// Populates the vector with every line in the file (ie every file name we'll be going to next)
+		fileNames = readFile(indexFile);
 
-		// Sort fileNames in alphabetical order
+		// Sort fileNames in alphabetical order.
+		//This is necessary for the correct sorting of the terms and proper display.
 		sort(fileNames.begin(), fileNames.end());
 	}
 	catch (const char* message) {
-		//if the file doesn't open for some reason, it'll display this message instead 
+		// If the file doesn't open it'll display this message instead
 		cerr << message << endl;
-	}
-
-	//this is just to test and see what's in the vector. It'll print each element. TO-DO: DELETE LATER
-	for (vector<string>::const_iterator i = fileNames.begin(); i != fileNames.end(); ++i) {
-		cout << *i << endl;
 	}
 	// PART 1: END
 	
 
 	// PART 2: PROCESSING
-	cout << "DISPLAYING WHAT'S IN EACH FILE STARTS HERE" << endl << endl;
-
-	// Stop words
+	// Process stop words file
 	string stopWordFilename = "stopwords.txt";
 	vector<string> stopWordList;
+	// Read words from the stop words list
+	stopWordList = readFile(stopWordFilename);
 
-	// File processing
+	// File processing and storing
 	string currentFile;
-
-	//vector<dictionary> init = std::vector::vector();
 	vector<dictionary> allTokensDict;
 	vector<dictionary> filteredTokensDict;
-
 	// Will get the length of the longest word for both dictionaries
 	string::size_type longestWordLength = 0;
 	string::size_type longestFilteredWordLength = 0;
 
-	// Read words from the stop words list
-	stopWordList = readFile(stopWordFilename);
 
 	try{
-		//iterate through the vector containing each file name
+		// Iterate through the vector containing each file name
 		for (vector<string>::const_iterator i = fileNames.begin(); i != fileNames.end(); ++i){
-			//file we're looking at is stored here
+			// File we're looking at is updated here
 			currentFile = *i;
 
 			// Open the file and process every word.
-			// Returns all tokens and a filtered tokens dictionary.
-			openingFiles(currentFile, stopWordList, longestWordLength, longestFilteredWordLength, allTokensDict, filteredTokensDict);
-
-			// PART 2: END
-
-
+			processFile(currentFile, stopWordList, longestWordLength, longestFilteredWordLength,
+					allTokensDict, filteredTokensDict);
 		}
 	}
 	catch (const char* message){
@@ -274,10 +303,12 @@ int main(){
 
 	allTokensDict 		= sortDict(allTokensDict);
 	filteredTokensDict 	= sortDict(filteredTokensDict);
-
+	// PART 2: END
 
 	// PART 3: OUTPUT
+	cout << "DICTIONARY CONTAINING ALL TOKENS:" << endl;
 	output(allTokensDict, fileNames, longestWordLength);
+	cout << "FILTERED DICTIONARY CONTAINING NO STOP WORDS:" << endl;
 	output(filteredTokensDict, fileNames, longestFilteredWordLength);
 	// PART 3: END
 

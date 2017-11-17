6fde044fa97a6c2194e1c31a26b072ba8b011f16
Samuel Dufresne <samuel.dufresne@live.com>
2017-11-17 11:59:49 -0500

    A4 init
    Next step is to refactor the code

---
+++ assign4/src/indexer.cpp
@@ -0,0 +1,8 @@
+/*
+ * indexer.cpp
+ *
+ *  Created on: Nov 5, 2017
+ *      Author: Maude
+ */
+
+#include "indexer.h"
---
+++ assign4/src/indexer.h
@@ -0,0 +1,21 @@
+/*
+ * indexer.h
+ *
+ *  Created on: Nov 5, 2017
+ *      Author: Maude
+ */
+
+#ifndef INDEXER_H_
+#define INDEXER_H_
+
+#include "index_item.h"
+
+class indexer {
+public:
+	struct query_result {
+		index_item doc;
+		double score;
+	};
+};
+
+#endif /* INDEXER_H_ */
